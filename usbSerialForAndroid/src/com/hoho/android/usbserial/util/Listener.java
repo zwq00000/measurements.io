b1c3a6376badc776f03e2bf324706cd633767f47
zwq00000 <zwq00000@gmail.com>
2014-08-03 15:04:23 +0800

    合并 Usb Serial for android

---
+++ usbSerialForAndroid/src/com/hoho/android/usbserial/util/Listener.java
@@ -0,0 +1,19 @@
+package com.hoho.android.usbserial.util;
+
+import java.nio.ByteBuffer;
+
+/**
+* Created by zwq00000 on 2014/7/26.
+*/
+public interface Listener {
+    /**
+     * Called when new incoming data is available.
+     */
+    public void onNewData(ByteBuffer data);
+
+    /**
+     * Called when {@link com.hoho.android.usbserial.util.SerialInputOutputManager#run()} aborts due to an
+     * error.
+     */
+    public void onRunError(Exception e);
+}
